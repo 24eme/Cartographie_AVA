072499d0b8c4edb72a4fe900b47be75d97704daf
victoiremarlet <victoire.marlet@orange.fr>
2016-08-08 16:47:54 +0200

    Add files via upload

---
+++ formulaire_rapide.php
@@ -0,0 +1,21 @@
+﻿<form action="result_formulaire.php" method="post">
+
+		<fieldset>
+		<h3> Formulaire de recherche</h3>
+		 <strong>Commune</strong>
+	<input type="text" name="commune" placeholder="Nom (en majuscule)"/>
+	
+	<p></p>
+	
+ <strong>Section</strong>
+	<input type="text" name="section" placeholder="N° de section"/>
+	
+	<p></p>
+	 <strong>Parcelle</strong>
+	<input type="text" name="parcelle" placeholder="N° de parcelle"/>
+	<p><br><input type="submit" name="envoi" value="Rechercher une parcelle"/></br></p>
+		
+		</fieldset>
+		
+		</form>
+		  
---
+++ result_formulaire.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>Cartographie AVA </title>
+	<meta charset="utf-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	
+</head>
+<body>
+	
+				<?php
+				include ('Cartographie_AVA.php');
+		
+		?>
+				
+			<?php
+		
+			if(isset($_POST["commune"]) && isset($_POST["section"]) && isset($_POST["parcelle"]))
+			{
+				$dbconn=pg_connect("host = localhost port = 5432 dbname=AVA user=postgres password=postgres");
+			$nomville=$_POST['commune'];
+			$section=$_POST['section'];
+			$parcelle=$_POST['parcelle'];
+			$result=pg_query($dbconn, "SELECT count(*) from \"BR_FORM\" where \"NomVille\"  = '$nomville' and sec='$section' and parc=$parcelle");
+			
+			$result_f=pg_fetch_array($result);
+			
+			
+			if( ! $result)
+			{
+				echo "Problème lors de la requête SQL";
+			}
+			else
+				{
+				if($result_f['count']==0)
+				{
+				echo "<h3>Aucun résultats ne correspond à votre recherche!</h3>"	;
+				}
+				else{
+					$result2=pg_query($dbconn, "SELECT \"NomVille\", sec, parc from \"BR_FORM\" where \"NomVille\"  = '$nomville'  and sec='$section' and parc=$parcelle");
+					
+					
+				while($result2_f=pg_fetch_array($result2))
+				{
+					
+					echo "<p>".$result2_f['NomVille'].' '.$result2_f['sec'].' '.$result2_f['parc']."</p>";
+					
+				}
+			}
+				}
+			}
+				?>
+			
+			
+		
+		
+</body>
+</html>

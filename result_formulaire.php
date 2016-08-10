60d5623d90d1c06e39410c07be64b6836506fe44
victoiremarlet <victoire.marlet@orange.fr>
2016-08-10 16:24:18 +0200

    Update result_formulaire.php

--- result_formulaire.php
+++ result_formulaire.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Cartographie AVA </title>
@@ -16,14 +16,14 @@
 		?>
 				
 			<?php
-		
+			echo "<h3>Formulaire Bas-Rhin</h3>";
 			if(isset($_POST["commune"]) && isset($_POST["section"]) && isset($_POST["parcelle"]))
 			{
 				$dbconn=pg_connect("host = localhost port = 5432 dbname=AVA user=postgres password=postgres");
 			$nomville=$_POST['commune'];
 			$section=$_POST['section'];
 			$parcelle=$_POST['parcelle'];
-			$result=pg_query($dbconn, "SELECT count(*) from \"BR_FORM\" where \"NomVille\"  = '$nomville' and sec='$section' and parc=$parcelle");
+			$result=pg_query($dbconn, "SELECT count(*) from \"BR_FORM\" where nomville = '$nomville' and section='$section' and tex='$parcelle'");
 			
 			$result_f=pg_fetch_array($result);
 			
@@ -39,13 +39,49 @@
 				echo "<h3>Aucun résultats ne correspond à votre recherche!</h3>"	;
 				}
 				else{
-					$result2=pg_query($dbconn, "SELECT \"NomVille\", sec, parc from \"BR_FORM\" where \"NomVille\"  = '$nomville'  and sec='$section' and parc=$parcelle");
+					$result2=pg_query($dbconn, "SELECT nomville, section, tex, x, y from \"BR_FORM\" where nomville  = '$nomville'  and section='$section' and tex='$parcelle'");
 					
 					
 				while($result2_f=pg_fetch_array($result2))
 				{
 					
-					echo "<p>".$result2_f['NomVille'].' '.$result2_f['sec'].' '.$result2_f['parc']."</p>";
+					echo "<p>".$result2_f['nomville'].' '.$result2_f['section'].' '.$result2_f['tex'].' '.$result2_f['x'].' '.$result2_f['y']."</p>";
+					
+				}
+			}
+				}
+			}
+				
+			echo "<h3>Formulaire Haut-Rhin</h3>";
+			if(isset($_POST["commune"]) && isset($_POST["section"]) && isset($_POST["parcelle"]))
+			{
+				$dbconn=pg_connect("host = localhost port = 5432 dbname=AVA user=postgres password=postgres");
+			$nomville=$_POST['commune'];
+			$section=$_POST['section'];
+			$parcelle=$_POST['parcelle'];
+			$result=pg_query($dbconn, "SELECT count(*) from \"HR_FORM\" where nomville = '$nomville' and section='$section' and numero='$parcelle'");
+			
+			$result_f=pg_fetch_array($result);
+			
+			
+			if( ! $result)
+			{
+				echo "Problème lors de la requête SQL";
+			}
+			else
+				{
+				if($result_f['count']==0)
+				{
+				echo "<h3>Aucun résultats ne correspond à votre recherche!</h3>"	;
+				}
+				else{
+					$result2=pg_query($dbconn, "SELECT nomville, section, numero, x, y from \"HR_FORM\" where nomville  = '$nomville'  and section='$section' and numero='$parcelle'");
+					
+					
+				while($result2_f=pg_fetch_array($result2))
+				{
+					
+					echo "<p>".$result2_f['nomville'].' '.$result2_f['section'].' '.$result2_f['numero'].' '.$result2_f['x'].' '.$result2_f['y']."</p>";
 					
 				}
 			}

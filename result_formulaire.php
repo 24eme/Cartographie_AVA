ff08cfc763e8ab7338352369137f279dcab9f7aa
Petit <mathurinpetit@users.noreply.github.com>
2016-08-10 17:21:36 +0200

    Update result_formulaire.php

--- result_formulaire.php
+++ result_formulaire.php
@@ -80,6 +80,32 @@
 					
 				while($result2_f=pg_fetch_array($result2))
 				{
+					// Ici par exemple tu pourrais écrire tout l'équivalent de index.php genre
+					
+					echo '<!DOCTYPE html>
+<html>
+<head>
+	<title>Cartographie AVA </title>
+	<meta charset="utf-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css" /> <!-- style Leaflet-->
+        ....';
+        //et puis à un moment
+        echo "		var map = L.map('map', {
+			center: [" . $result2_f['x'].' '. . ", " . $result2_f['y'] .  " ],
+			zoom: 8,
+			layers: [streets, cities]
+		});"
+		 //et puis le reste j'usqu'à la fin
+		 echo '
+</script> <!-- fin de script js-->
+	
+	</body>
+</html>';
+		
 					
 					echo "<p>".$result2_f['nomville'].' '.$result2_f['section'].' '.$result2_f['numero'].' '.$result2_f['x'].' '.$result2_f['y']."</p>";
 					
